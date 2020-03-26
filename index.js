3dbc8e468dd66e828f3a987178456a092a11d530
drospond <david.rospond@gmail.com>
2020-03-25 22:57:06 -0400

    Started inquirer prompt

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 var api = require("./utils/api.js")
-
-const questions = [
-
-];
-
+var generateMarkdown = require("./utils/generateMarkdown");
+var fs = require("fs");
+var inquirer = require("inquirer");
+//
+inquirer.prompt({
+    message: "Enter your GitHub username:",
+    name: "username"
+}).then(function({username}){
+    api.getUser(username);
+})
+//
 function writeToFile(fileName, data) {
-}
 
+}
+//
 function init() {
 
 }
 
 init();
 
-api.getUser();
--- utils/api.js
+++ utils/api.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const api = {
   getUser(username) {
     // const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
-    const queryUrl = "https://api.github.com/users/drospond/";
+    const queryUrl = `https://api.github.com/users/${username}`;
     axios.get(queryUrl).then(function(res) {
       console.log(res.data.avatar_url);
       console.log(res.data.email);
